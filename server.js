08f3e283f8e92047a21e819882b0f8aab066fbd8
Shubham Gautam <sg2199203@gmail.com>
2021-05-02 14:00:02 +0530

    Adding functionality

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const app = express()
-const axios = require('axios')
 const bodyParser = require('body-parser')
 const path = require('path')
-const port = process.env.PORT || 8000
+const port = process.env.PORT || 3000
+const axios = require('axios')
+const urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
@@ -11,7 +12,12 @@ app.set('views', path.join(__dirname, 'views'))
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.get('/', (req, res) => {
-    res.render('index')
+    res.render('index.ejs')
+})
+
+app.post('/vaccine', urlencodedParser, (req, res) => {
+    //add a function to access API
+    res.render('vaccine.ejs', {data: req.body})
 })
 
 app.listen(port, () => {
